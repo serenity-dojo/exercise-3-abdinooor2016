51f9f216886c12f6714a9eb6fd9c01f22094fdac
Abdi Noor <abdinooor@gmail.com>
2023-06-20 17:31:14 +0100

    Completed exersice 3 Classes and Objects

--- src/test/java/com/serenitydojo/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -1,7 +1,27 @@
 package com.serenitydojo;
 
+import com.serenitydojo.checks.Dog;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class WhenCreatingObjects {
+
+    @Test
+    public void creating_a_dog() {
+        String name = "Fido";
+        String favoriteFood = "Bone";
+        int age = 5;
+
+        Dog fido = new Dog();
+
+        fido.setName("Fido");
+        System.out.println(fido.getName());
+
+        fido.setfavoriteFood("Bone");
+        System.out.println(fido.getfavoriteFood());
+
+        fido.setAge(5);
+        System.out.println(fido.getAge());
+    }
+
 }
---
+++ src/test/java/com/serenitydojo/checks/Dog.java
@@ -0,0 +1,34 @@
+package com.serenitydojo.checks;
+
+public class Dog {
+
+    public String name;
+
+    public String favoriteFood;
+
+    public int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String  getName() {
+        return name;
+    }
+
+    public void setfavoriteFood(String favoriteFood) {
+        this.favoriteFood = favoriteFood;
+    }
+
+    public String  getfavoriteFood() {
+        return favoriteFood;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
